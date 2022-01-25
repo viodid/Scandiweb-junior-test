d3cbc43933ef78dee453a92f5521ea1b9bdaeed1
viodid <david.yunta.aller@gmail.com>
2022-01-25 13:33:03 +0000

    sass styles

---
+++ app/database/Database.php
@@ -0,0 +1,37 @@
+<?php
+
+require_once '/etc/scandiweb-junior-developer-test-task/config.php';
+
+class Database
+{
+    private $host = HOST;
+    private $db_name = DB_NAME;
+    private $user = USER;
+    private $pass = PASS;
+    private $conn;
+
+    // DB Connect
+    public function connect()
+    {
+        try {
+            $this->conn = new \PDO(
+                "mysql:host=$this->host;dbname=$this->db_name",
+                $this->user,
+                $this->pass
+            );
+            // foreach ($this->conn->query('SELECT * from products') as $row) {
+            //     print_r($row);
+            // }
+            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $this->conn->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
+        } catch (PDOException $e) {
+            print "Error!: " . $e->getMessage() . "<br/>";
+            die();
+        }
+
+        return $this->conn;
+    }
+}
+
+// $test = new Database();
+// $test->connect();
