a630eaa0e512cb8726818292839ef7d8f93aed1d
viodid <david.yunta.aller@gmail.com>
2022-01-26 22:39:16 +0000

    start index fetch read

--- public/index.php
+++ public/index.php
@@ -16,29 +16,45 @@
             <h1>Product Page</h1>
             <div class="btn-cnt">
                 <button type="button" onclick="location.href='/addproduct'" class="btn primary">ADD</button>
-                <button type="submit" form="product_form" class="btn secondary">MASS DELETE</button>
+                <button type="submit" form="index_form" id="delete-product-btn" class="btn secondary">MASS DELETE</button>
             </div>
         </div>
     </header>
     <hr>
     <main>
-        <?php include '../app/controllers/read.php';
-        while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
-            foreach ($row as $key => $value) {
-                echo "{$key} => {$value} ";
+        <form action="../app/controllers/delete.php" method="get" class="form-product" id="index_form">
+            <?php include '../app/controllers/read.php';
+            // Check if any posts
+            if ($num > 0) {
+
+                while ($row = $result->fetch(PDO::FETCH_OBJ)) {
+                    // var_dump($row);
+            ?>
+
+                    <div class="product-container">
+                        <input type="checkbox" name="id1.test" class=".delete-checkbox">
+                        <div class="attributes">
+                            <?php
+                            var_dump($row);
+                            ?>
+                        </div>
+                    </div>
+
+            <?php
+
+                }
             }
-            echo var_dump($row[0]);
-        }
 
 
 
 
 
-        // $superResult = $result->fetch(PDO::FETCH_ASSOC);
-        // echo $superResult['SKU'];
-        // $second = $result->fetch(PDO::FETCH_ASSOC);
-        // echo $second['SKU'];
-        ?>
+            // $superResult = $result->fetch(PDO::FETCH_ASSOC);
+            // echo $superResult['SKU'];
+            // $second = $result->fetch(PDO::FETCH_ASSOC);
+            // echo $second['SKU'];
+            ?>
+        </form>
     </main>
     <hr>
     <footer>
